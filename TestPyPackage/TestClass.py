fdadd7164b3a8734b0c3b1efda0bf7ae62811e12
ljchu <aljchu@163.com>
2014-02-20 08:43:20 +0800

    First commit.

---
+++ TestPyPackage/TestClass.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+'''
+    @author: ljchu
+    @warning: Nothing
+    2014-2-19
+'''
+class TestClass():
+    '''Pring a string.'''
+    
+    def __init__(self,aString):
+        self.aString = aString
+    
+    def print_a_string(self):
+        print(self.aString)
+    
---
+++ setup.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+      name = 'TestPyPackage',
+      packages = ['TestPyPackage'],
+      version = '1.0.1',
+      description = 'Print a string',
+      author = 'ljchu',
+      author_email='aljchu@163.com',
+      url='http://github.com/ljchu/CIINFO',
+      download_url='',
+      keyword=['TestPyPackage'],
+      classifiers=[
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Development Status :: 5 - Production/Stable",
+                   "Environment :: x",
+                   "Framework :: x",
+                   "Intended Audience :: Developers",
+                   "Topic :: Intetnet :: WWW/HTTP",
+                   "Topic :: Intetnet :: WWW/HTTP :: Dynamic Content",
+                   "Topic :: Intetnet :: WWW/HTTP :: WSGI",
+                   "Topic :: Software Development :: Libraries ::Python",
+                   "Modules",
+                   ]
+      )
